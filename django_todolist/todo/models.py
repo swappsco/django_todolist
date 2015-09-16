33c595574921a64bec79de5ca72a62c22d09eb94
andresgz <andresgz@gmail.com>
2015-09-16 18:51:41 -0500

    Add Python portability to model

--- django_todolist/todo/models.py
+++ django_todolist/todo/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Todo(models.Model):
     """
     Todo Model: name, description, created
@@ -9,5 +11,5 @@ class Todo(models.Model):
     description = models.TextField()
     created = models.DateTimeField()
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
