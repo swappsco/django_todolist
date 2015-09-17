d8854a95afec3846e05f4cb9be8673aa5a1b1288
andresgz <andresgz@gmail.com>
2015-09-17 00:30:39 -0500

    Add AngularJS base app

---
+++ front/app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name frontApp
+ * @description
+ * # frontApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('frontApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
